3cb737a0866525d43da2a4c83295fb424e46e6f3
sekiguchi <sekiguchi@hoge.com>
2016-10-18 18:27:38 +0900

    warビルドを可能に変更

--- webapp001/src/main/java/org/test/web/App.java
+++ webapp001/src/main/java/org/test/web/App.java
@@ -1,9 +1,16 @@
 package org.test.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class App {
+public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AppConfiguration.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppConfiguration.class);
 	}
 }
