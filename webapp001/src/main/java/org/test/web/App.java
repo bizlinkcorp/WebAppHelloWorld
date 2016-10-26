4ef6e2a83a75080296347c2892980fdec18a9ead
sekiguchi <sekiguchi@hoge.com>
2016-10-26 11:00:05 +0900

    コメントを入れました

--- webapp001/src/main/java/org/test/web/App.java
+++ webapp001/src/main/java/org/test/web/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
+		// コメントを入れました
 		SpringApplication.run(AppConfiguration.class, args);
 	}
 
