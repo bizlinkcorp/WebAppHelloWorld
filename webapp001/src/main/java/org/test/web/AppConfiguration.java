2a6baa1c8666dd186efede604c96eb08e8e1b218
sekiguchi <sekiguchi@test.com>
2016-12-15 11:39:41 +0900

    起動設定メンテナンス

--- webapp001/src/main/java/org/test/web/AppConfiguration.java
+++ webapp001/src/main/java/org/test/web/AppConfiguration.java
@@ -3,7 +3,10 @@ package org.test.web;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan({"org.test.web"})
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
+@ComponentScan({ "org.test.web" })
 public class AppConfiguration {
 }
